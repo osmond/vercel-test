0cfe81b8041cfc7d4038a86016c403b3a533766b
osmond <j@osmond.cc>
2025-07-30 13:32:11 -0500

    initial vercel test

--- api/index.py
+++ api/index.py
@@ -1,10 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps({"message": "Hello from Vercel Python"}).encode())
-        return
+def handler(req, res):
+    res.status_code = 200
+    res.headers["Content-Type"] = "application/json"
+    res.write(json.dumps({"message": "Hello from Python"}))
